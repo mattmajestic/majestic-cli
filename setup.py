3fa505a2034f8a11da07c9c2da6f6c9bd05f8d17
Matt Majestic <33904170+mattmajestic@users.noreply.github.com>
2024-07-22 09:38:46 -0400

    update for action

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ setup(
     name='majestic-cli',
     version='0.1.0',
     packages=find_packages(),
-    py_modules=['majestic-cli'],
+    py_modules=['majestic_cli'],
     install_requires=[
         'click',
     ],
     entry_points='''
         [console_scripts]
-        majestic-cli=majestic-cli:cli
+        majestic-cli=majestic_cli:cli
     ''',
 )
